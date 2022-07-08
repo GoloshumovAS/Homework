4bb884a8ccc540d411fc5498668ef374269f82b6
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 15:43:30 +0300

    Add files via upload

---
+++ Homework6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waitingTime = 10;
+            Console.WriteLine("Сколько людей в очереди?");
+            int peopleInLine = Convert.ToInt32(Console.ReadLine());
+            int timeInHours = waitingTime * peopleInLine / 60;
+            int timeInMinutes = waitingTime * peopleInLine % 60;
+            Console.WriteLine($"Время ожидания в очереди {timeInHours} час(а)" +
+                $" {timeInMinutes} минут");
+
+        }
+    }
+}
