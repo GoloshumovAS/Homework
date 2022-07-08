afd092a645abc031b1d864a87d8771d4f7ac874e
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 15:32:29 +0300

    Add files via upload

---
+++ Homework5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceCrystal = 10;
+
+            Console.WriteLine("Введите кол-во золота:");
+            int countGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько кристаллов вы хотите купить?");
+            int countCrystal = Convert.ToInt32(Console.ReadLine());
+
+            countGold -= countCrystal * priceCrystal;
+
+            Console.WriteLine($"вы купили {countCrystal} кристалов" +
+                $" и у вас осталось {countGold} золота");
+        }
+    }
+}
