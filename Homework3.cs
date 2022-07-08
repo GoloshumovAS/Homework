54f4d820d4b469a5f70c62c5488e7b710dd3be77
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 14:52:29 +0300

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureCount = 52;
+            int rowSize = 3;
+            int availablePictures = pictureCount / rowSize;
+            int unusedPictures = pictureCount % rowSize;
+            Console.WriteLine($"Полностю заполненных рядов: {availablePictures}," +
+                $" неиспользуемых картинок: {unusedPictures}");
+        }
+    }
+}
