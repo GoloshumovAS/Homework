6ada4200a64f6dec6181c8b953e520dd7cfa5b2c
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 15:07:39 +0300

    Add files via upload

---
+++ Homework4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Ivanov";
+            string userSurname = "Ivan";
+            Console.WriteLine($"Ваше имя: {userName}, ваша фамилия {userSurname}");
+            string buffer = userName;
+            userName = userSurname;
+            userSurname = buffer;
+            Console.WriteLine($"Ваше имя: {userName}, ваша фамилия {userSurname}");
+        }
+    }
+}
