f240af3e02c9a58df4dc5a751c1c5d5f0466db7d
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 14:31:37 +0300

    Add files via upload

---
+++ Homework2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите вашу профессию: ");
+            string userProfession = Console.ReadLine();
+            Console.WriteLine("Какой цвет ваш любимый?");
+            string userFavoriteColor = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {userName}, вам {userAge} лет," +
+                $" по специальности вы {userProfession} и ваш любимый цвет {userFavoriteColor}.");
+        }
+    }
+}
