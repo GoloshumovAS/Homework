fe2e21aec2737fb01a603ca6e5d90255430f0aa3
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 13:30:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte buttonState; 
+            int playerHealth;
+            int numberOfItems;
+            float luckRatio;
+            float surfaceTemperature;
+            double critChance;
+            bool checkHit;
+            bool enoughCoins;
+            string locationName;
+            string dialogueOption;
+        }
+    }
+}
