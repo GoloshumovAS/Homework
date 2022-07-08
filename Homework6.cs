d9ba12705eb3896dc999998fc54c6b1e3dda9d58
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-08 19:48:21 +0300

    Update Homework6.cs

--- Homework6.cs
+++ Homework6.cs
@@ -7,13 +7,13 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             int waitingTime = 10;
+            int minutesInHours = 60;
             Console.WriteLine("Сколько людей в очереди?");
             int peopleInLine = Convert.ToInt32(Console.ReadLine());
-            int timeInHours = waitingTime * peopleInLine / 60;
-            int timeInMinutes = waitingTime * peopleInLine % 60;
+            int timeInHours = waitingTime * peopleInLine / minutesInHours;
+            int timeInMinutes = waitingTime * peopleInLine % minutesInHours;
             Console.WriteLine($"Время ожидания в очереди {timeInHours} час(а)" +
                 $" {timeInMinutes} минут");
-
         }
     }
 }
