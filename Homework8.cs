61a9d50bd05917c02312d4950a69f5dd8dbacebd
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-10 22:09:38 +0300

    Add files via upload

---
+++ Homework8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i);
+                i++;
+                Console.Write("Введите exit что бы остановить цикл: ");
+                string exiteWord = Console.ReadLine();
+                if (exiteWord == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
