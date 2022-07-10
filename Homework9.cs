407e5300ffae77e4c9d1a1604647ab3be872834c
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-10 22:31:17 +0300

    Update Homework9.cs

--- Homework9.cs
+++ Homework9.cs
@@ -2,4 +2,17 @@
 
 namespace ConsoleApp11
 {
+      class Program
+    {
+        static void Main()
+        {
+            int number = 7;
+            int numberCount;
+
+            for (numberCount = 5; numberCount <= 96; numberCount += number)
+            {
+                Console.WriteLine(numberCount);
+            }
+        }
+    }
 }
