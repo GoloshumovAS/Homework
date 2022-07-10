e6fbae0ad98de73a8310fe672e8f7b7a8861b218
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-10 20:59:26 +0300

    Add files via upload

---
+++ Homework7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string inputWord = Console.ReadLine();
+            Console.WriteLine("Введите число повторений");
+            int wordCount = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < wordCount; i++)
+            {
+                Console.WriteLine(inputWord);
+            }
+        }
+    }
+}
