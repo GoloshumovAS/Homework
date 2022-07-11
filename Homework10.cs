78abea663247ddb0da0192dfc99ff51b8636a92d
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-11 09:57:25 +0300

    Update Homework10.cs

--- Homework10.cs
+++ Homework10.cs
@@ -8,11 +8,13 @@ namespace ConsoleApp12
         {
             Random rand = new Random();
             int number = rand.Next(0, 100);
+            int firstNumberDivisor = 3;
+            int secondNumberDivisor = 5;
             int numberSum = 0;
 
             for(int i = 0; i <= number; i++)
             {
-                if(i % 3 == 0 || i % 5 == 0)
+                if(i % firstNumberDivisor == 0 || i % secondNumberDivisor == 0)
                 {
                     numberSum += i;
                 }
