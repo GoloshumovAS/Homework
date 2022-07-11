70b5db20cc71011c6a13a920e1f9b4166b2792d7
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-11 09:45:57 +0300

    Add files via upload

---
+++ Homework10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+            int numberSum = 0;
+
+            for(int i = 0; i <= number; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    numberSum += i;
+                }
+            }
+
+            Console.WriteLine(numberSum);
+        }
+    }
+}
