7e85a8e683be602af326e4bf09405a08bfa08a25
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-11 16:36:47 +0300

    Add files via upload

---
+++ Homework14.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework14
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfAttempts = 2;
+            string userPassword = "qwerty123";
+            Console.Write("Введите пароль:");
+            string inputPassword = Console.ReadLine();
+
+            for(int i = 0; i < numberOfAttempts; i++)
+            {
+                if(inputPassword == userPassword)
+                {
+                    Console.WriteLine("Тут было секретное сообщение...\n" +
+                        "нажмите любую клавишу что бы выйти");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный пароль. У вас осталось {numberOfAttempts - i} попыток.");
+                    inputPassword = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
