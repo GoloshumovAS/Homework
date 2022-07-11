b26d60b39c03dafab745b3e77ac1e23babac5f29
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-11 21:26:39 +0300

    Update Homework15.cs

--- Homework15.cs
+++ Homework15.cs
@@ -4,21 +4,25 @@ namespace Homework15
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main()
         {
             int maximumMultiple = 999;
             int lengthNuber = 3;
-            Random rand = new Random();
-            int randomNumber = rand.Next(0, 28);
+            int randomStart = 0;
+            int randomFinish = 28;
+            int countNumber = 0;
+            Random random = new Random();
+            int randomNumber = random.Next(randomStart, randomFinish);
             Console.WriteLine(randomNumber);
 
             for (int i = randomNumber; i < maximumMultiple; i += randomNumber)
             {
                 if(Convert.ToString(i).Length >= lengthNuber)
                 {
-                    Console.WriteLine(i);
+                    countNumber++;
                 }
             }
+            Console.WriteLine(countNumber);
         }
     }
 }
