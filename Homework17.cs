fd6454a1dee09a5173b6a7c5203d1dd90da7275a
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-12 16:02:54 +0300

    Add files via upload

---
+++ Homework17.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework17
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Введите строку из ( и ):");
+            string stringBracket = Console.ReadLine();
+            const char braketLeft = '(';
+            int summBraketLeft = 0;
+            const char braketRight = ')';
+            int summBraketRight = 0;
+            foreach (char allBraket in stringBracket)
+            {
+                switch (allBraket)
+                {
+                    case braketLeft:
+                        summBraketLeft++;
+                        break;
+                    case braketRight:
+                        summBraketRight++;
+                        break;
+                }
+            }
+            if (summBraketLeft == summBraketRight)
+            {
+                Console.WriteLine($"Строка {stringBracket} корректная и максимум глубины является {summBraketLeft}.");
+            }
+        }
+    }
+}
