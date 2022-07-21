3905865e8e869dcbbdfbcbebc124750e30c9d7c4
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-21 23:04:26 +0300

    Update Homework22.cs

--- Homework22.cs
+++ Homework22.cs
@@ -1,56 +1,50 @@
-﻿using System;
-
+using System;
 namespace Homework22
 {
-    class Program
+    class Homework22
     {
         static void Main()
         {
-            int[] arrayNumbers = new int[1];
-            int[] arrayNumbersChange = new int[arrayNumbers.Length + 1];
-            int sumArray = 0;
+            int[] arrayNumbers = new int[0];
             string commandSum = "sum";
             string commandExit = "exit";
             bool stopLoop = true;
-            int userNumber;
             int counter = 0;
-            string userInput;
-
-            Console.WriteLine("Введите число в массив," +
-                " или команду 'sum' для получения суммы элементов массива," +
-                " или команду 'exit' для выхода ");
-
+            Console.WriteLine($"Введите число в массив," +
+                $" или команду {commandSum} для получения суммы элементов массива," +
+                $" или команду {commandExit} для выхода ");
+            
             while (stopLoop)
             {
+                string userInput = Console.ReadLine();
 
-                userInput = Console.ReadLine();
                 if(userInput == commandExit)
                 {
                     stopLoop = false;
-                    break;
                 }
-                if(userInput == commandSum)
+
+                else if(userInput == commandSum)
                 {
-                    for(int i = 0; i < arrayNumbers.Length; i++)
+                    int sumArray = 0;
+                    
+                    for (int i = 0; i < arrayNumbers.Length; i++)
                     {
                         sumArray += arrayNumbers[i];
                     }
-
+                    
                     Console.WriteLine($"сумма элементов массива = {sumArray}");
-                    arrayNumbers = new int[1];
-                    counter = 0;
-                    sumArray = 0;
                 }
+
                 else
                 {
-                    arrayNumbersChange = new int[arrayNumbers.Length + 1];
-                    userNumber = Convert.ToInt32(userInput);
-                    
+                    int userNumber = Convert.ToInt32(userInput);
+                    int[] arrayNumbersChange = new int[arrayNumbers.Length + 1];
+
                     for (int i = 0; i < arrayNumbers.Length; i++)
                     {
                         arrayNumbersChange[i] = arrayNumbers[i];
                     }
-                   
+
                     arrayNumbers = arrayNumbersChange;
                     arrayNumbers[counter] = userNumber;
                     counter++;
