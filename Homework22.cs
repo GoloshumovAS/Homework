77c1ce8cf642a2b7e6cb1a16892d62a4c9c4acdc
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-22 18:48:57 +0300

    Update Homework22.cs

--- Homework22.cs
+++ Homework22.cs
@@ -5,49 +5,46 @@ namespace Homework22
     {
         static void Main()
         {
-            int[] arrayNumbers = new int[0];
+            int[] array = new int[0];
             string commandSum = "sum";
             string commandExit = "exit";
-            bool stopLoop = true;
-            int counter = 0;
+            bool isRun = true;
             Console.WriteLine($"Введите число в массив," +
                 $" или команду {commandSum} для получения суммы элементов массива," +
                 $" или команду {commandExit} для выхода ");
-            
-            while (stopLoop)
+
+            while (isRun)
             {
                 string userInput = Console.ReadLine();
 
                 if(userInput == commandExit)
                 {
-                    stopLoop = false;
+                    isRun = false;
                 }
 
                 else if(userInput == commandSum)
                 {
                     int sumArray = 0;
-                    
-                    for (int i = 0; i < arrayNumbers.Length; i++)
+                    for (int i = 0; i < array.Length; i++)
                     {
-                        sumArray += arrayNumbers[i];
+                        sumArray += array[i];
                     }
-                    
+
                     Console.WriteLine($"сумма элементов массива = {sumArray}");
                 }
 
                 else
                 {
                     int userNumber = Convert.ToInt32(userInput);
-                    int[] arrayNumbersChange = new int[arrayNumbers.Length + 1];
-
-                    for (int i = 0; i < arrayNumbers.Length; i++)
+                    int[] arrayChange = new int [array.Length + 1];
+                    
+                    for (int i = 0; i < array.Length; i++)
                     {
-                        arrayNumbersChange[i] = arrayNumbers[i];
+                        arrayChange[i] = array[i];
                     }
-
-                    arrayNumbers = arrayNumbersChange;
-                    arrayNumbers[counter] = userNumber;
-                    counter++;
+                    
+                    arrayChange[arrayChange.Length - 1] = userNumber;
+                    array = arrayChange;
                 }
             }
         }
