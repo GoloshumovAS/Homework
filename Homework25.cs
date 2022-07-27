e3a64478dc96b753c7c6b0eadec5fc992935a63f
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-07-27 22:36:48 +0300

    Update Homework25.cs

--- Homework25.cs
+++ Homework25.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-//Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.
 namespace Homework25
 {
     class Program
@@ -8,6 +7,7 @@ namespace Homework25
         {
             string text = "Точки исчезли из подстрок, однако теперь появились две дополнительные пустые подстроки.";
             string[] array = text.Split(' ');
+            
             for(int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
