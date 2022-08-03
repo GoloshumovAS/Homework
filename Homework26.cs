4c4852d9e62d69b5d4e6aaa2393b2569472a35b6
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-08-03 12:26:16 +0300

    Add files via upload

---
+++ Homework26.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//Дан массив чисел. Нужно его сдвинуть циклически на указанное пользователем значение позиций влево, не используя других массивов. 
+//Пример для сдвига один раз: { 1, 2, 3, 4} => { 2, 3, 4, 1}
+namespace Homework26
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = {1,2,3,4,5};
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Укажите смещение массива на кол-во позиций: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < number; i++)
+            {
+                int temp = array[0];
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+                array[^1] = temp;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+    }
+}
