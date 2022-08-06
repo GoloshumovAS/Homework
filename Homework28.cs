3bf9ec31a0216c7f5502f22782ccc99d9c176cdb
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-08-06 21:18:34 +0300

    Add files via upload

---
+++ Homework28.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework28
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.SetCursorPosition(0, 5);
+            Console.WriteLine("Введите процент заполненности шкалы здоровья:");
+            int userPercent = Convert.ToInt32(Console.ReadLine());
+            userPercent /= 10;
+            Console.SetCursorPosition(10, 0);
+            PaintHealthbar(userPercent);
+        }
+        static void PaintHealthbar(int size)
+        {
+            char[] array = new char[10];
+            Console.Write('[');
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = '#';
+                Console.Write(array[i]);
+            }
+                for (int i = size; i < array.Length; i++)
+            {
+                array[i] = '_';
+                Console.Write(array[i]);
+            }
+
+            Console.Write(']');
+        }
+    }
+}
