a7af5982d50091080681dbedd118014c469523b6
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-08-17 21:23:20 +0300

    Add files via upload

---
+++ Homework29.cs
@@ -0,0 +1,45 @@
+﻿using System;
+//Написать функцию, которая запрашивает число у пользователя (с помощью метода Console.ReadLine() ) и пытается сконвертировать его в тип int (с помощью int.TryParse())
+//Если конвертация не удалась у пользователя запрашивается число повторно до тех пор, пока не будет введено верно. 
+//После ввода, который удалось преобразовать в число, число возвращается.
+//P.S Задача решается с помощью циклов
+//P.S Также в TryParse используется модфикатор параметра out
+namespace Homework29
+{
+    class Homework29
+    {
+        static void Main()
+        {
+            bool startLoop = true;
+
+            while (startLoop)
+            {
+                Console.WriteLine("Введите число");
+                string userInput = Console.ReadLine();
+                Number(userInput, ref startLoop);
+                if (startLoop != true)
+                {
+                    Console.WriteLine($"Число {userInput} сконвертированно");
+                }
+            }
+        }
+        static void Number(string userNumber, ref bool runLoop)
+        {
+
+            while (runLoop)
+            {
+                bool convertResult = int.TryParse(userNumber, out int number);
+
+                if (convertResult == true)
+                {
+                    runLoop = false;
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте ввести другое число");
+                    break;
+                }
+            }
+        }
+    }
+}
