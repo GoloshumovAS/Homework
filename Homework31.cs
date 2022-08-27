6653ae344f4fd7c325ef0dbc076e44ade091faea
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-08-27 09:10:34 +0300

    Update Homework31.cs

--- Homework31.cs
+++ Homework31.cs
@@ -1,18 +1,20 @@
-﻿using System;
+using System;
 //Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
 namespace Homework31
 {
-    class Program
+    class Homework31
     {
         static void Main(string[] args)
         {
             int[] array = new int[5];
             Random random = new Random();
+
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = random.Next(0, array.Length);
                 Console.Write(array[i]);
             }
+
             Console.WriteLine();
             Shuffle(array);
         }
@@ -20,12 +22,13 @@ namespace Homework31
         static void Shuffle(int[] array)
         {
             Random random = new Random();
+
             for (int i = array.Length - 1; i >= 0; i--)
             {
                 int j = random.Next(i);
-                var temp = array[j];
+                int arrayElement = array[j];
                 array[j] = array[i];
-                array[i] = temp;
+                array[i] = arrayElement;
                 Console.Write(array[i]);
             }
         }
