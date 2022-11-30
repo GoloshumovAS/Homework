561f4bb35bb14ad60fc61e9453718147c25b9209
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2022-11-30 14:55:46 +0600

    Update Homework32.cs

--- Homework32.cs
+++ Homework32.cs
@@ -11,25 +11,26 @@ namespace Homework32
         {
             Console.WriteLine("Введите слово - ");
             string userWord = Console.ReadLine().ToLower();
+
             string key1 = "c";
             string key2 = "java";
             string key3 = "javascript";
             string answer1 = "Вы сделали лошадь";
             string answer2 = "Вы очень хотите сделать лошадь";
             string answer3 = "Вам прислали лошадь в разных пакетах";
+
             Dictionary<string, string> question = new Dictionary<string, string>();
             question.Add(key1,answer1 );
             question.Add(key2,answer2 );
             question.Add(key3,answer3 );
 
-            GetAnswer(userWord, question);
+            CheckWord(userWord, question);
         }
-        
-        static void GetAnswer(string word, Dictionary<string,string> question)
+        static void CheckWord(string word, Dictionary<string,string> question)
         {
             foreach(var item in question)
             {
-                if(item.Key == word)
+                if(question.ContainsKey(word))
                 {
                     Console.WriteLine($"ваш ответ:  {item.Value}");
                     break;
