7d2f9815aa2cbf8d2c53860781b5573c7b744c30
GoloshumovAS <100596288+GoloshumovAS@users.noreply.github.com>
2023-02-09 17:41:33 +0600

    Add files via upload

---
+++ Homework37.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework37
+{
+    class Homework37
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.PlayerName = "Jhon Doe";
+            player.PlayerLevel = 20;
+            player.PlayerClass = "Mage";
+            player.ShowInfo();
+        }
+    }
+
+
+    class Player 
+    {
+        public string PlayerName;
+        public int PlayerLevel;
+        public string PlayerClass;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Карточка игрока: \nимя - " + PlayerName + "\nуровень - " + PlayerLevel + "\nкласс - " + PlayerClass);
+        }
+    }
+}
